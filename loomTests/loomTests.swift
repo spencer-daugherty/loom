5d1f2ea0780ddefcc5c1a2a0130bbf076b89d6d1
Spencer Daugherty <89596474+spencer-daugherty@users.noreply.github.com>
2025-04-28 20:18:48 -0400

    Initial Commit

---
+++ loomTests/loomTests.swift
@@ -0,0 +1,17 @@
+//
+//  loomTests.swift
+//  loomTests
+//
+//  Created by Spencer Daugherty on 4/28/25.
+//
+
+import Testing
+@testable import loom
+
+struct loomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
